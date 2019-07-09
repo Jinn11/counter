78089a858a6238f1e9b1518e752b5814dd25c4c5
Jinn11 <51450479+Jinn11@users.noreply.github.com>
2019-07-09 13:42:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+
+  constructor () {
+
+    super();
+
+    this.state = {
+
+      counter: 0,
+
+      double: false,
+
+    }
+
+  }
+
+increment = () => {
+
+  if(this.state.counter < 20) {
+
+    this.setState({
+
+      counter: this.state.counter + 1
+
+    })
+
+  }
+
+  if(this.state.double){
+
+    if(this.state.counter < 19){
+
+      this.setState({
+
+        counter: this.state.counter + 2
+
+      })
+
+    }
+
+  }
+
+};
+
+decrement = () => {
+
+  if(this.state.counter > 0) {
+
+    this.setState({
+
+      counter: this.state.counter -1,
+
+    })
+
+  }
+
+  if(this.state.double) {
+
+    if(this.state.counter > 1) {
+
+      this.setState({
+
+        counter: this.state.counter -2
+
+      })
+
+    }
+
+  }
+
+}
+clear = () => {
+
+  this.setState({
+
+    counter: 0,
+
+  })
+
+}
+double = () => {
+
+  if(this.state.double) {
+
+    this.setState({
+
+      double: false,
+
+    })
+
+  } else {
+
+    this.setState({
+
+      double: true
+
+    })
+
+  }
+
 }
 
-export default App;
+  render() {
+
+    console.log("This is line 13 representating state", this.state.counter);
+
+    console.log("This is the double value", this.state.double);
+
+    return(
+
+      <div className="container">
+
+        <div className="navbar">Counter.js</div>
+
+        <div className="counter">
+
+          <h1>{this.state.counter}</h1>
+
+          <button type="button" onClick={this.clear}>Clear Count</button>
+
+          <button type="button" onClick={this.increment}>Increment</button>
+
+          <button type="button" onClick={this.decrement}>Decrement</button>
+
+          <button type="button" onClick={this.double}>{this.state.double ? 'Double Count' : "Single Count"}</button>
+
+        </div>
+
+      </div>
+
+    )
+
+  }
+
+}
+export default Counter;
