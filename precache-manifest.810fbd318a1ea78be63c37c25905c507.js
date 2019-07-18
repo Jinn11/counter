23bdef3e718c2c8a0586bdb3126e506604da71f5
Mohammad Jinnah <maj74865@gmail.com>
2019-07-18 14:51:49 -0400

    Updates

---
+++ precache-manifest.810fbd318a1ea78be63c37c25905c507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04290221dd273cb80df764d1a822cd42",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "2af050a2f005b0b7fd0f",
+    "url": "/counter/static/css/main.d4b51fd0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2af050a2f005b0b7fd0f",
+    "url": "/counter/static/js/main.8db871bb.chunk.js"
+  },
+  {
+    "revision": "395f42efa7c50e4ddc67",
+    "url": "/counter/static/js/runtime~main.16ddd954.js"
+  }
+]);
